48c1f20d30daf1426340943f332efb217b202b22
Sasa-Indeed <ahmed382002@gmail.com>
2022-12-04 19:11:47 +0000

    added the no spaces function in main

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include "Tree.h"
 #include <string>
+#include <algorithm>
+#include <cctype>
+
+std::string deleteSpaces(std::string expression);
 
 int main() {
     TreeType<std::string> tr;
-    tr.insert("((aa+69)*(c/(d-1896)))",0);
+
+
+    tr.insert("((a+b)*(c/(d-e)))");
     tr.displayInorder();
     std::cout<<'\n';
     tr.displayPostorder();
     std::cout<<'\n';
     tr.displayPreorder();
     return 0;
+}
+
+std::string deleteSpaces(std::string expression) {
+    expression.erase(std::remove_if(expression.begin(), expression.end(), ::isspace),
+            expression.end());
+
+    return expression;
 }
